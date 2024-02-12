5a76c8290e38f86e6c1564f9f103390fe7f64022
Skand-xor <skand.sisodia@xorlabs.com>
2024-02-12 15:18:47 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var nameList = [
+    "Ayush",
+    "Skand",
+    "Tushar",
+    "Shubh",
+    "Pulkit",
+    "Rahul",
+    "Sachin",
+    "Rohit",
+    "Shiv",
+    "Piyush"
+  ];
+
+  String getCharater(String name) {
+    return name[0];
+  }
+
+  dialog(String content) {
+    showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+              title: const Text("Alert Dialog box"),
+              content: Text(content),
+              actions: [
+                TextButton(
+                  child: const Text(
+                    "Okay",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  onPressed: () => Navigator.of(ctx).pop(),
+                )
+              ],
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black45,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Center(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              child: const Text(
+                'Home',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () => dialog("You have pressed Home button."),
+            ),
+            TextButton(
+              child: const Text(
+                'Video Conference',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () =>
+                  dialog("You have pressed Video Conference button."),
+            ),
+            TextButton(
+              child: const Text(
+                'Questionnaier',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () => dialog("You have pressed Questionnaier button."),
+            ),
+            TextButton(
+              child: const Text(
+                'Profile',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () => dialog("You have pressed Profile button."),
+            )
+          ],
+        )),
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(top: 50, left: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              width: 400,
+              height: 1000,
+              margin: const EdgeInsets.only(bottom: 50),
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Text(nameList[index][0]),
+                    ),
+                    title: Text(
+                      nameList[index],
+                      style: const TextStyle(fontFamily: 'protestRevolution'),
+                    ),
+                    subtitle: Text(nameList[index]),
+                    trailing: InkWell(
+                      child: const Icon(Icons.add, color: Colors.black),
+                      onTap: () => dialog("${nameList[index]}, add icon click"),
+                    ),
+                  );
+                },
+                itemCount: nameList.length,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 50),
+              child: SingleChildScrollView(
+                child: Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  child: const Column(
+                    children: [
+                      SizedBox(
+                        height: 200,
+                        width: 200,
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                              backgroundImage:
+                                  AssetImage("assets/images/groot.jpg")),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 200,
+                        width: 200,
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                              backgroundImage:
+                                  AssetImage("assets/images/asus.jpg")),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 200,
+                        width: 200,
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                              backgroundImage:
+                                  AssetImage("assets/images/banner.jpg")),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 200,
+                        width: 200,
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                              backgroundImage:
+                                  AssetImage("assets/images/groot.jpg")),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
