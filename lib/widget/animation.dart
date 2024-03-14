280eeeb5bebfc56dc865eb0fdc1adc3f99d0c8d6
Skand-xor <skand.sisodia@xorlabs.com>
2024-03-14 18:22:05 +0530

    Add opacity animation

--- lib/widget/animation.dart
+++ lib/widget/animation.dart
@@ -49,3 +49,51 @@ class _BoxAnimationState extends State<BoxAnimation> {
     );
   }
 }
+
+class OpacityAnimation extends StatefulWidget {
+  const OpacityAnimation({super.key});
+
+  @override
+  State<OpacityAnimation> createState() => OpacityAnimationState();
+}
+
+class OpacityAnimationState extends State<OpacityAnimation> {
+  var _opacity = 1.0;
+  bool flag = true;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AnimatedOpacity(
+            duration: const Duration(seconds: 2),
+            curve: Curves.bounceInOut,
+            opacity: _opacity,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(12)),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                if (flag) {
+                  _opacity = 0.3;
+                } else {
+                  _opacity = 1.0;
+                }
+                setState(() {
+                  flag = !flag;
+                });
+              },
+              child: const Text("Animate"))
+        ],
+      ),
+    );
+  }
+}
